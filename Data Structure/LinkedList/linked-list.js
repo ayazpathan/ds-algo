a25d4b4e29306f01ca68f680e1d474ba27be052e
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-01-12 12:37:03 +0530

    Worked on create, append and prepend operations in linked list

---
+++ Data Structure/LinkedList/linked-list.js
@@ -0,0 +1,36 @@
+// 1->10->8->15
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+console.log(myLinkedList);
