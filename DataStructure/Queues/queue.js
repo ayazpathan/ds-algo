37a8fafa3485713a10c246b2ffee5eb88dc1c19f
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-02-15 22:58:37 +0530

    Working on queue data structure

---
+++ DataStructure/Queues/queue.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {}
+  enqueue(value) {}
+  dequeue() {}
+  //isEmpty;
+}
+
+const myQueue = new Queue();
+
+//Joy
+//Matt
+//Pavel
+//Samir
