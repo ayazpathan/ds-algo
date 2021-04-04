d3c20579d2a4a23e7241d4f36cec5b425aa9e25a
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-04-04 12:11:15 +0530

    Finished queue and and Tree DS as well

--- DataStructure/Queues/queue.js
+++ DataStructure/Queues/queue.js
@@ -18,18 +18,40 @@ class Queue {
   enqueue(value) {
     // Add an element in the queue
 
-    /*
-      addding more comments here
-    */
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
   }
   dequeue() {
     // Remove the element from the queue
+    if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return this;
   }
   //isEmpty;
 }
 
 const myQueue = new Queue();
-
+myQueue.enqueue("Aiyazkhan");
+myQueue.enqueue("Arbaz");
+myQueue.enqueue("Shahbaz");
+myQueue.enqueue("Mohsin");
+myQueue.dequeue();
+console.log(myQueue);
 //Joy
 //Matt
 //Pavel
